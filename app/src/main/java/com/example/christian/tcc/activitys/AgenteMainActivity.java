371a0522bb49417ceacd50b683ae0c65643667dd
Christian Matos <matos.christianm@gmail.com>
2018-04-17 15:47:16 -0300

    commit dia 17/04, location ok

--- app/src/main/java/com/example/christian/tcc/activitys/AgenteMainActivity.java
+++ app/src/main/java/com/example/christian/tcc/activitys/AgenteMainActivity.java
@@ -1,8 +1,14 @@
 package com.example.christian.tcc.activitys;
 
+import android.Manifest;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,12 +18,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.christian.tcc.R;
 import com.example.christian.tcc.config.ChecaSegundoPlano;
 import com.example.christian.tcc.config.ConfiguracaoFirebase;
 import com.example.christian.tcc.config.CustomFirebaseInstanceIDService;
 import com.example.christian.tcc.config.MyFirebaseMessagingService;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -32,6 +40,8 @@ public class AgenteMainActivity extends AppCompatActivity {
     private TextView txtReceberPedidos;
     private FirebaseAuth mAuth;
     private boolean clicou = true;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +59,6 @@ public class AgenteMainActivity extends AppCompatActivity {
         txtReceberPedidos = (TextView) findViewById(R.id.txt_receber_pedidos);
         txtReceberPedidos.setText("Você poderá receber um pedido de acompanhamento a qualquer momento.");
 
-
         btnReceberPedidos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,7 +66,70 @@ public class AgenteMainActivity extends AppCompatActivity {
             }
         });
 
+        pedirPermissoes();
+
     }
+
+    private void pedirPermissoes() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        else
+            configurarServico();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    configurarServico();
+                } else {
+                    Toast.makeText(this, "É preciso permitir o acesso à localização!!!", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void configurarServico(){
+        try {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    atualizar(location);
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                public void onProviderEnabled(String provider) { }
+
+                public void onProviderDisabled(String provider) { }
+            };
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }catch(SecurityException ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void atualizar(Location location) {
+        Double latPoint = location.getLatitude();
+        Double lngPoint = location.getLongitude();
+
+        if(usuarioLogado!=null) {
+
+            usuarioLogado.setLatitude(latPoint);
+            usuarioLogado.setLongitude(lngPoint);
+            usuarioLogado.salvar();
+        }
+    }
+
 
 
     public void receberPedidos(){
